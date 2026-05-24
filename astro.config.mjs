3897479bf8433bd3eab2c000293c8ae307ca241d
Ziyao Wang <93502916+WZY10065@users.noreply.github.com>
2026-05-24 17:28:04 +0800

    Add sitemap integration for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://ziyaowang100-star.github.io",
+  site: "https://ziyaowang100-star.github.io/academic-homepage",
   base: "/academic-homepage",
   integrations: [
+    sitemap(),
     tailwind({
       applyBaseStyles: false
     })
